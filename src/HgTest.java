ac10ff7db8f48c167556e660039f02b48560c693
Karun <none@none>
2012-08-23 11:01:54 +1000

    Minor change from Karun

--- src/HgTest.java
+++ src/HgTest.java
@@ -5,7 +5,7 @@ public class HgTest {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-System.out.println("KARUN.");
+System.out.println("KARUN agarwal .");
 System.out.println("Tasneem :).");
 
 System.out.println("Hao");
