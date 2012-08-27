f3414800a55f254ad8d711b881c8719441dcdfe2
Hao Fei <u5074277@anu.edu.au>
2012-08-27 13:05:28 +1000

    refactoring: rewrite Main.java in the Javafx builder-style
    --HG--
    branch : hao/demo

--- src/info/crystalsnow/comp1110/demo/Main.java
+++ src/info/crystalsnow/comp1110/demo/Main.java
@@ -1,40 +1,43 @@
 package info.crystalsnow.comp1110.demo;
 
-import javax.swing.border.TitledBorder;
+import java.util.ArrayList;
+import java.util.Collection;
 
 import javafx.application.Application;
 import javafx.application.Platform;
+
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
-import javafx.geometry.Insets;
-import javafx.geometry.Orientation;
+
 import javafx.scene.Group;
+import javafx.scene.GroupBuilder;
 import javafx.scene.Scene;
+import javafx.scene.SceneBuilder;
+
 import javafx.scene.control.Button;
+import javafx.scene.control.ButtonBuilder;
 import javafx.scene.control.Menu;
 import javafx.scene.control.MenuBar;
+import javafx.scene.control.MenuBarBuilder;
+import javafx.scene.control.MenuBuilder;
 import javafx.scene.control.MenuItem;
+import javafx.scene.control.MenuItemBuilder;
 import javafx.scene.control.SeparatorMenuItem;
-import javafx.scene.control.TitledPane;
-import javafx.scene.effect.DropShadow;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
-import javafx.scene.input.MouseEvent;
+
 import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.FlowPane;
-import javafx.scene.layout.TilePane;
-import javafx.scene.layout.VBox;
+import javafx.scene.layout.BorderPaneBuilder;
+
 import javafx.scene.paint.Color;
+
 import javafx.stage.Stage;
 
-
 public class Main extends Application {
+	
 	private class _CloseAppHandler implements EventHandler<ActionEvent> {
 		@Override
 		public void handle(ActionEvent event) {
 				Platform.exit();
 		}
-		
 		
 	}
     public static void main(String[] args)
@@ -44,43 +47,48 @@ public class Main extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
-        Group root = new Group();
-    	Scene scene = new Scene(root, 800, 600, Color.WHITE);
-    	scene.getStylesheets().add(this.getClass().getResource(
-    		      "/resources/stylesheet.css"
-    		    ).toExternalForm());
-
     	
-    	// Create Menu Bar and make its width auto-adjustable
-    	MenuBar menuBar = new MenuBar();
+    	Menu fileMenu = MenuBuilder.create()
+    			.text("Demo")
+                .items(
+                        MenuItemBuilder.create().text("New").build()
+                        , MenuItemBuilder.create().text("Save").build()
+                        , new SeparatorMenuItem()
+                        , MenuItemBuilder.create().text("Exit")
+                            .onAction(new _CloseAppHandler()).build()
+                      )
+    			.build();
+    	
+    	MenuBar menuBar = MenuBarBuilder.create().menus(fileMenu).build();
     	menuBar.prefWidthProperty().bind(stage.widthProperty());
     	
-    	Menu fileMenu = new Menu("file");
-    	fileMenu.getItems().add(new MenuItem("New"));
-    	fileMenu.getItems().add(new MenuItem("Save"));
-    	fileMenu.getItems().add(new SeparatorMenuItem());
-    	MenuItem exitMenuItem = new MenuItem("Exit");
-    	exitMenuItem.setOnAction(new _CloseAppHandler());
+    	Button exitButton = ButtonBuilder.create()
+    			.text("Exit")
+    			.id("exitButton")
+    			.maxHeight(Double.MAX_VALUE)
+    			.onAction(new _CloseAppHandler())
+    			.build();
+    	exitButton.maxWidthProperty().bind(stage.widthProperty().multiply(0.77));
     	
-    	fileMenu.getItems().add(exitMenuItem);
-    	menuBar.getMenus().add(fileMenu);
-    
-// not work, the image shows like an icons instead of a background image
-//		Image imageDecline = new Image(getClass().getResourceAsStream("button.png"));
-//    	exitButton.setGraphic(new ImageView(imageDecline));
-    	final Button exitButton = new Button("Exit");
-    	exitButton.setMaxSize(Double.MAX_VALUE, Double.MAX_VALUE);
-    	exitButton.setOnAction(new _CloseAppHandler());
-    	exitButton.setId("exitButton");
+    	BorderPane borderPane = BorderPaneBuilder.create()
+    			.center(exitButton)
+    			.top(menuBar) 
+    			.build();
     	
-    	BorderPane borderPane = new BorderPane();
-    	borderPane.setCenter(exitButton);
-    	borderPane.setTop(menuBar);
-    	
-    	root.getChildren().add(borderPane);
-
-    	stage.setTitle("Demo");
+    	Group root = GroupBuilder.create().children(borderPane).build();    	
+        Scene scene = SceneBuilder.create()
+            .root(root)
+            .stylesheets(
+                    this.getClass().getResource(
+                        "/resources/stylesheet.css"
+                        ).toExternalForm())
+            .width(800)
+            .height(600)
+            .fill(Color.WHITE)
+            .build();
+        
     	stage.setScene(scene);
+    	stage.setTitle("Demo");
         stage.show();
     }
 }
