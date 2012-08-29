e758eaee27d1c9a8a864bd47384a2ee6e5fb1c80
karun <none@none>
2012-08-29 23:49:31 +1000

    Trying to create an object of gris and have the object being passed to MainFrame
    --HG--
    branch : dev

--- src/tetrisGame/MainFrame.java
+++ src/tetrisGame/MainFrame.java
@@ -1,21 +1,41 @@
 package tetrisGame;
 
 import javafx.application.Application;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
 
 public class MainFrame extends Application {
 
 	@Override
-	public void start(Stage arg0) throws Exception {
-		// TODO Auto-generated method stub
-
+	public void start(Stage primaryStage) throws Exception {
+		
+		primaryStage.setTitle("Tetris");
+		Group root = new Group();
+		Scene scene = new Scene(root, 600, 500);
+		primaryStage.setScene(scene);
+		
+		for(int i=1;i<=22;i++){
+			for(int j=1;j<=10;j++){
+					
+				Rectangle r = new Rectangle(j*20, i*20, 20, 20);
+				r.setFill(Color.LIGHTGRAY);
+				r.setStroke(Color.BLACK);
+				r.setStrokeWidth(0.5);
+				root.getChildren().add(r);
+			}
+		}
+		
+	
+		
+		primaryStage.show();
 	}
 
-	/**
-	 * @param args
-	 */
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	launch(args);
 
 	}
 
