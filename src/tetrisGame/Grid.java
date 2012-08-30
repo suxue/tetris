93f437d736c8bbd32ece2e2b8ff8ab5012e85407
karun <none@none>
2012-08-30 11:00:06 +1000

    Grid object passed to MainFrame
    --HG--
    branch : dev

--- src/tetrisGame/Grid.java
+++ src/tetrisGame/Grid.java
@@ -1,22 +1,28 @@
 package tetrisGame;
 
-import javafx.application.Application;
+
+import javafx.scene.Group;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
 
-public class Grid extends Application {
+public class Grid {
 
-	@Override
-	public void start(Stage arg0) throws Exception {
+	Group root =new Group();
+	public Group drawGrid(){
+		for(int i=1;i<=22;i++)
+			for(int j=1;j<=10;j++){
+				
+				Rectangle r = new Rectangle(j*20, i*20, 20, 20);
+				r.setFill(Color.LIGHTGRAY);
+				r.setStroke(Color.BLACK);
+				r.setStrokeWidth(0.5);
+				root.getChildren().add(r);
+		}
+		return(root);
+	}
 		
 
-	}
-
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
+	
 
 }
--- src/tetrisGame/MainFrame.java
+++ src/tetrisGame/MainFrame.java
@@ -16,19 +16,12 @@ public class MainFrame extends Application {
 	public void start(Stage primaryStage) throws Exception {
 		
 		primaryStage.setTitle("Tetris");
-		Group root = new Group();
+		Grid g=new Grid();
+        Group root = g.drawGrid();
 		Scene scene = new Scene(root, 600, 500,Color.BLUEVIOLET);
 		primaryStage.setScene(scene);
 		
-		for(int i=1;i<=22;i++){
-			for(int j=1;j<=10;j++){
-					
-				Rectangle r = new Rectangle(j*20, i*20, 20, 20);
-				r.setFill(Color.LIGHTGRAY);
-				r.setStroke(Color.BLACK);
-				r.setStrokeWidth(0.5);
-				root.getChildren().add(r);
-			}
+
 			Rectangle r2 = new Rectangle(315, 100, 200, 100);
 			r2.setFill(Color.LIGHTSEAGREEN);
 			r2.setStroke(Color.GRAY);
@@ -39,13 +32,13 @@ public class MainFrame extends Application {
 			t1.setFont(Font.font("Tahoma", FontWeight.NORMAL, 25));
 			t1.setTranslateX(350);
 			t1.setTranslateY(85);
-			root.getChildren().add(t1);		}
+			root.getChildren().add(t1);		
 		
-			Rectangle r2 = new Rectangle(315, 275, 200, 30);
-			r2.setFill(Color.LIGHTSEAGREEN);
-			r2.setStroke(Color.GRAY);
-			r2.setStrokeWidth(2);
-			root.getChildren().add(r2);
+			Rectangle r3 = new Rectangle(315, 275, 200, 30);
+			r3.setFill(Color.LIGHTSEAGREEN);
+			r3.setStroke(Color.GRAY);
+			r3.setStrokeWidth(2);
+			root.getChildren().add(r3);
 			
 			Text t2 = new Text("LEVEL");
 			t2.setFont(Font.font("Tahoma", FontWeight.NORMAL, 25));
@@ -53,11 +46,11 @@ public class MainFrame extends Application {
 			t2.setTranslateY(260);
 			root.getChildren().add(t2);	
 			
-			Rectangle r3 = new Rectangle(315, 350, 200, 30);
-			r3.setFill(Color.LIGHTSEAGREEN);
-			r3.setStroke(Color.GRAY);
-			r3.setStrokeWidth(2);
-			root.getChildren().add(r3);
+			Rectangle r4 = new Rectangle(315, 350, 200, 30);
+			r4.setFill(Color.LIGHTSEAGREEN);
+			r4.setStroke(Color.GRAY);
+			r4.setStrokeWidth(2);
+			root.getChildren().add(r4);
 		
 			Text t3 = new Text("SCORE");
 			t3.setFont(Font.font("Tahoma", FontWeight.NORMAL, 25));
