9f8044b19d1be9cf9a318cdb9aba9298d921ab36
karun <none@none>
2012-09-27 10:38:38 +1000

    changes in Direction class
    --HG--
    branch : dev

--- src/test_Tetris/Direction.java
+++ src/test_Tetris/Direction.java
@@ -1,27 +1,14 @@
 package test_Tetris;
 
 public enum Direction {
+    RIGHT,LEFT;
     
-    package _11_game;
 
-    import javafx.event.EventHandler;
-    import javafx.scene.input.KeyCode;
-    import javafx.scene.input.KeyEvent;
-
-   
-        boolean isHorizontal() {
+  
+        boolean isVertical() {
             return (this == LEFT || this == RIGHT);
         }
-        
-        /**
-         * @return true if the direction is in the increasing direction
-         */
-        boolean isIncreasing() {
-            return (this == DOWN || this == RIGHT);
-        }
-    }
-    
-
-    
-
+       
 }
+    
+
