3988a918264c254bf880a63cfadf526bb3ec09ed
Tasneem <none@none>
2012-10-03 23:38:11 +1000

    minor change
    --HG--
    branch : stable

--- src/tetris/tetrominos/WallKick.java
+++ src/tetris/tetrominos/WallKick.java
@@ -5,12 +5,51 @@ package tetris.tetrominos;
 public class WallKick extends Cell {
     
     
-    public boolean canRotateRight(double width) {
+    public boolean canRotateRightI(double width) {
         return canMoveRight(width);
-
         }
-    public boolean canRotateLeft (double width){
-        return canMoveLeft (width);
-    }
+    
+    public boolean canRotateRightJ(double width) {
+        return canMoveRight(width);
+        }
+    public boolean canRotateRightL(double width) {
+        return canMoveRight(width);
+        }
+    public boolean canRotateRightO(double width) {
+        return canMoveRight(width);
+        }
+    public boolean canRotateRightZ(double width) {
+        return canMoveRight(width);
+        }
+    public boolean canRotateRightT(double width) {
+        return canMoveRight(width);
+        }
+    public boolean canRotateRightS(double width) {
+        return canMoveRight(width);
+        }
+    
+    
+    
+    public boolean canRotateLeftI(double width) {
+        return canMoveRight(width);
+        }
+    public boolean canRotateLeftJ(double width) {
+        return canMoveRight(width);
+        }
+    public boolean canRotateLeftL(double width) {
+        return canMoveRight(width);
+        }
+    public boolean canRotateLeftO(double width) {
+        return canMoveRight(width);
+        }
+    public boolean canRotateLeftZ(double width) {
+        return canMoveRight(width);
+        }
+    public boolean canRotateLeftT(double width) {
+        return canMoveRight(width);
+        }
+    public boolean canRotateLeftS(double width) {
+        return canMoveRight(width);
+        }
 }
 //http://tetrisconcept.net/wiki/SRS#Wall_Kicks
