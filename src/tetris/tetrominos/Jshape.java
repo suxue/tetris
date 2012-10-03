7ae12479ceb4dda34206c023d868b2fe3353084b
karun <none@none>
2012-10-03 14:02:20 +1000

    Created Jshape
    --HG--
    branch : stable

---
+++ src/tetris/tetrominos/Jshape.java
@@ -0,0 +1,35 @@
+package tetris.tetrominos;
+
+public class Jshape extends SimpleTetromino {
+
+    @Override
+    public double getPivotXShift() {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    @Override
+    public double getPivotYShift() {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    @Override
+    public boolean moveDown(double len) {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    @Override
+    public boolean moveLeft() {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    @Override
+    public boolean moveRight() {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+}
