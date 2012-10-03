b56c6610322af5cfbca90fbb5b23f3bae18ec9bf
Tasneem <none@none>
2012-10-03 22:04:18 +1000

    created WallKick class
    --HG--
    branch : stable

---
+++ src/tetris/tetrominos/WallKick.java
@@ -0,0 +1,15 @@
+package tetris.tetrominos;
+
+
+
+public class WallKick extends Cell {
+    
+    
+    public boolean canRotateRight() {
+        return false;
+
+        }
+    public boolean canRotateLeft (){
+        return false;
+    }
+}
