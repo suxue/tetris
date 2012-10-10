2ac02367a0594b56cd734bc4bb626f0a2b9b9927
Hao Fei <u5074277@anu.edu.au>
2012-10-10 15:20:17 +1100

    remove redundant css
    --HG--
    branch : dev

--- src/tetris/core/Tetris.java
+++ src/tetris/core/Tetris.java
@@ -223,14 +223,9 @@ public final class Tetris extends TetrisDynamic implements GameControl {
             cssurl = Tetris.class.getResource("/css/stylesheet.css");
         }
 
-        URL fallbackCssUrl = Tetris.class.getResource("/css/fallback-stylesheet.bss");
-        if (fallbackCssUrl == null) {
-            fallbackCssUrl = Tetris.class.getResource("/css/fallback-stylesheet.css");
-        }
 
         Scene primaryScene = SceneBuilder.create()
                 .root(new RootUI(this))
-                .stylesheets(fallbackCssUrl.toExternalForm())
                 .stylesheets(cssurl.toExternalForm())
                 .width(getWidth())
                 .height(getHeight())
