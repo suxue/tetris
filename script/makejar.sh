ec4e313e81ed11a0af310a220575d6617aa5a086
Hao Fei <u5074277@anu.edu.au>
2012-10-16 01:55:04 +1100

    update build script
    --HG--
    branch : dev

--- script/makejar.sh
+++ script/makejar.sh
@@ -1,4 +1,11 @@
 #!/bin/sh
 
+rm -f game.jar
+
 javafxpackager  -createjar -nocss2bin -appclass tetris.core.Main -srcdir . \
--outdir .. -outfile game -v \
+-outdir . -outfile game -v 
+
+pack200 --repack --effort=9 --segment-limit=-1 --modification-time=latest \
+--strip-debug ./game.jar
+
+
